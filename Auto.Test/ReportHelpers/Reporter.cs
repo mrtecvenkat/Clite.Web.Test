a9c9df87ea7cd4530853f34bdbc8645068dd4a2b
Venkata Kosanam <mrtec.in@gmail.com>
2018-07-26 22:53:42 -0500

    Started Extenet Reports

--- Auto.Test/ReportHelpers/Reporter.cs
+++ Auto.Test/ReportHelpers/Reporter.cs
@@ -1,15 +1,49 @@
-﻿using System;
+﻿using Auto.Test.GenericHelpers;
+using Auto.Test.Tool.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RelevantCodes.ExtentReports;
 
 namespace Auto.Test.ReportHelpers
 {
-    public class Reporter
+    public class Reporter : DriverManager
     {
-        public static void LogErrorMassage(string msg)
+        private static ExtentReports report;
+        private static ExtentTest curtest;
+        public static void StartReporting()
         {
+            string resultPath = Environment.CurrentDirectory + "\\" + TestDataHelper.GetValueFromAppSettings("TestResults") + "\\ExtentReport.html";
+            report = new ExtentReports(resultPath);
+        }
+        public static void EndReporting()
+        {
+            report.Flush();
+            report.Close();
+        }
+        public static void StartTestReporting(string testcase, string description = "")
+        {
+            curtest = report.StartTest(testcase);
+
+        }
+        public static void EndTestReporting()
+        {
+            report.EndTest(curtest);
+        }
+
+        public static void LogFailMassage(string msg, string testcaseid = "")
+        {
+            Random rnd = new Random();
+            string screenShortName = testcaseid + ".png";
+            int randomItem = rnd.Next(1, 999999);
+            if (testcaseid == "")
+                screenShortName = randomItem + ".png";
+            string resultPath = TestDataHelper.GetValueFromAppSettings("TestResults");
+            DriverManager.TakeScreenshot(resultPath + "\\" + screenShortName);
+
+            curtest.Log(LogStatus.Fail, msg);
 
         }
         public static void LogPassMassage(string msg)
